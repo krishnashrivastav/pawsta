e30d2b4c555fd2ac503de3674fe8d097e5f0f729
krishnashrivastav <krishnashrivastav23@gmail.com>
2022-01-22 00:22:47 +0530

    done with User Authentication for the App and the Post functionality

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, {useState, useEffect} from 'react';
 import './App.css';
 import Post from './Post';
-import { db } from './firebase';
+import { auth, db } from './firebase';
 import Modal from '@mui/material/Modal';
-import { Box, Button, makeStyles, Typography } from '@mui/material';
+import { Box, Button, Input, makeStyles, Typography } from '@mui/material';
+import ImageUpload from './ImageUpload';
 
 // function getModalStyle() {
 //   const top = 50;
@@ -46,38 +47,141 @@ function App() {
   const [open, setOpen] = React.useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
+
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState('');
+  const [user, setUser] = useState(null);
+  const [openSignIn, setOpenSignIn] = useState('');
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+        // user has logged in...
+        console.log(authUser);
+        setUser(authUser);
+      } else {
+        // user has logged out...        
+        setUser(null);
+      }
+    })
+
+    return () => {
+      //perform some cleanup actions
+      unsubscribe();
+    }
+
+  }, [user, username]);
+
   // const [modalStyle] = useState(getModalStyle);
   // const classes = useStyles();
 
+  const signUp = (event) => {
+    event.preventDefault();
+
+    auth
+    .createUserWithEmailAndPassword(email, password)
+    .then((authUser) => {
+      return authUser.user.updateProfile({
+        displayName: username
+      })
+    })
+    .catch((error) => alert(error.message))
+    setOpen(false);
+  }
+
+  const signIn = (event) => {
+    event.preventDefault();
+
+    auth
+      .signInWithEmailAndPassword(email, password)
+      .catch((error) => alert(error.message))
+
+    setOpenSignIn(false);
+  }
+
   useEffect(() => {
-    db.collection('posts').onSnapshot(snapshot => {
+    db.collection('posts').orderBy('timestap','desc').onSnapshot(snapshot => {
       setPosts(snapshot.docs.map(doc => ({
         id : doc.id,
         post : doc.data()
       })));
     })
   }, []);
-
+ 
   return (
-    <div className="app">
-
+    <div className="app">    
     <Modal
       open={open}
       onClose={handleClose} 
     >
 
     <Box sx={style}>
-      <Typography id="modal-modal-title" variant="h6" component="h2">
-        Text in a modal
-      </Typography>
-      <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-        Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-      </Typography>
+      <form className='app__signup'>
+      <center>
+        <img 
+          className='app__headerImage'
+          src = 'pawsta logo 1.png'
+          alt = ''
+        />
+        </center>
+        <Input
+          placeholder='username'
+          type='text'
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <Input
+          placeholder='email'
+          type='text'
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <Input
+          placeholder='password'
+          type='password'
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <Button type='submit' onClick={signUp}>Sign Up</Button>
+      </form>
     </Box>
 
       {/* <div style={modalStyle} className={classes.paper}>
         <h2>I am a Modal</h2>
       </div> */}
+    </Modal>  
+
+    <Modal
+      open={openSignIn}
+      onClose={() => setOpenSignIn(false)} 
+    >
+
+    <Box sx={style}>
+      <form className='app__signup'>
+      <center>
+        <img 
+          className='app__headerImage'
+          src = 'pawsta logo 1.png'
+          alt = ''
+        />
+        </center>
+        
+        <Input
+          placeholder='email'
+          type='text'
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <Input
+          placeholder='password'
+          type='password'
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <Button type='submit' onClick={signIn}>Sign In</Button>
+      </form>
+    </Box>
     </Modal>  
 
     <div className="app__header">
@@ -86,17 +190,28 @@ function App() {
         src='pawsta logo 1.png'
         alt=''      
       />
+
+    {user ? (
+      <Button onClick={() => auth.signOut()}>Logout</Button>
+    ): (
+      <div className='app__loginContainer'> 
+        <Button onClick={() => setOpenSignIn(true)}>Sign In</Button>
+        <Button onClick={() => setOpen(true)}>Sign Up</Button>
+      </div>
+    )}
+
     </div>
-
-    <Button onClick={() => setOpen(true)}>
-      Sign Up
-    </Button>
-
       { 
         posts.map(({id, post}) => (
           <Post key ={id} username={post.username} caption={post.caption} imageUrl={post.imageUrl}/>
         ))
       }
+
+    {user?.displayName ? (
+      <ImageUpload username={user.displayName} />
+    ) : (
+      <h3>Sorry, You need to Login to Upload</h3>
+    )}
 
       {/* <Post username='lucylife' caption='Heyyyy, Lucy here ;)' imageUrl='https://www.purina.com.au/-/media/project/purina/main/breeds/puppies/puppy-cocker-spaniel.jpg'/>
       <Post username='tom_thecat' caption='Heyyyy, Tom here ;)' imageUrl='https://s.w-x.co/in-cat_in_glasses.jpg'/>
